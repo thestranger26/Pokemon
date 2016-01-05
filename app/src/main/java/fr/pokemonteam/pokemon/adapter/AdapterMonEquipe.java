58acf3c76dbf7002cb308ddaae334d5d20a40f59
tristan.leuillier <thestranger26@hotmail.fr>
2016-01-05 13:58:13 +0100

    Ajout de la vue "Mon Equipe"

---
+++ app/src/main/java/fr/pokemonteam/pokemon/adapter/AdapterMonEquipe.java
@@ -0,0 +1,56 @@
+package fr.pokemonteam.pokemon.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import fr.pokemonteam.pokemon.R;
+import fr.pokemonteam.pokemon.model.PokemonReel;
+
+/**
+ * Created by Tristan on 05/01/16.
+ */
+public class AdapterMonEquipe extends ArrayAdapter<PokemonReel> {
+
+    int layoutId;
+
+    public AdapterMonEquipe(Context context, int resource, List<PokemonReel> objects) {
+        super(context, resource, objects);
+        layoutId = resource;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        PokemonReel pkm = getItem(position);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(layoutId, parent, false);
+        }
+        TextView pseudo = (TextView) convertView.findViewById(R.id.monEquipe_pseudoPkm);
+        TextView nom = (TextView) convertView.findViewById(R.id.monEquipe_nomPokemon);
+        TextView niv = (TextView) convertView.findViewById(R.id.monEquipe_niveauPkm);
+        TextView atk = (TextView) convertView.findViewById(R.id.monEquipe_atkPkm);
+        TextView def = (TextView) convertView.findViewById(R.id.monEquipe_defPkm);
+        TextView exp = (TextView) convertView.findViewById(R.id.monEquipe_expPkm);
+
+        niv.setText(Integer.toString(pkm.getNiveau()));
+        atk.setText(atk.getText() + Integer.toString(pkm.getAtk()));
+        def.setText(def.getText() + Integer.toString(pkm.getDef()));
+        exp.setText(exp.getText() + Integer.toString(pkm.getExp()));
+
+
+        if(pkm.getPseudo() == null || pkm.getPseudo().isEmpty()) {
+            nom.setText("");
+            pseudo.setText(pkm.getPokemon().getNom());
+        } else {
+            pseudo.setText(pkm.getPseudo());
+            nom.setText(pkm.getPokemon().getNom());
+        }
+
+        return convertView;
+    }
+}
--- app/src/main/java/fr/pokemonteam/pokemon/controller/MainActivity.java
+++ app/src/main/java/fr/pokemonteam/pokemon/controller/MainActivity.java
@@ -7,13 +7,9 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.UiSettings;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-
-import java.util.BitSet;
 
 import fr.pokemonteam.pokemon.R;
 import fr.pokemonteam.pokemon.bdd.Database;
