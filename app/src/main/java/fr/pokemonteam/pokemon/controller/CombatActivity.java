9ad1ba3d128d10e8120cdb8e7dcc5a80e748e5ae
tristandefournas <tristan.de-fournas@cpe.fr>
2016-01-05 17:32:32 +0100

    Combat OK ATK et DEF

--- app/src/main/java/fr/pokemonteam/pokemon/controller/CombatActivity.java
+++ app/src/main/java/fr/pokemonteam/pokemon/controller/CombatActivity.java
@@ -11,6 +11,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import fr.pokemonteam.pokemon.R;
 import fr.pokemonteam.pokemon.bdd.Database;
@@ -20,6 +21,8 @@ import fr.pokemonteam.pokemon.model.Utilisateur;
 
 public class CombatActivity extends AppCompatActivity {
 
+    PokemonReel pkmnCourant;
+    PokemonReel pkmnAdverse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,18 +35,19 @@ public class CombatActivity extends AppCompatActivity {
         System.out.println(u.getNom());
 
         ArrayList<PokemonReel> monEquipe = u.getEquipe();
-        PokemonReel pkmnCourant = monEquipe.get(0);
+        pkmnCourant = monEquipe.get(0);
+        pkmnAdverse = monEquipe.get(1);
 
         // Définition des infos du pokémon adverse
         ImageView imageViewAdverse = (ImageView) findViewById(R.id.imagePokemonAdverse);
-        imageViewAdverse.setImageResource(R.mipmap.magikarp);
+        imageViewAdverse.setImageResource(this.getResources().getIdentifier("magikarp", "mipmap", this.getPackageName()));
 
         ProgressBar progressBarAdverse = (ProgressBar) findViewById(R.id.progressBarPokemonAdverse);
         progressBarAdverse.setMax(100);
         progressBarAdverse.setProgress(100);
 
         TextView textAdverse = (TextView) findViewById(R.id.textPokemonAdverse);
-        textAdverse.setText("Magikarp");
+        textAdverse.setText(pkmnAdverse.getPseudo()  + " N." + pkmnAdverse.getNiveau());
 
         // Définition des infos de notre pokémon
         ProgressBar progressBarAllie = (ProgressBar) findViewById(R.id.progressBarPokemonAllie);
@@ -51,7 +55,7 @@ public class CombatActivity extends AppCompatActivity {
         progressBarAllie.setProgress(pkmnCourant.getVieActuelle());
 
         TextView textNomAllie = (TextView) findViewById(R.id.textNomPokemonAllie);
-        textNomAllie.setText(pkmnCourant.getPseudo());
+        textNomAllie.setText(pkmnCourant.getPseudo() + " N." + pkmnCourant.getNiveau());
 
         ImageView imageViewAllie = (ImageView) findViewById(R.id.imagePokemonAllie);
         imageViewAllie.setImageResource(R.mipmap.chrysacier);
@@ -60,5 +64,84 @@ public class CombatActivity extends AppCompatActivity {
         textVieAllie.setText(pkmnCourant.getVieActuelle() + "/" + pkmnCourant.getMaxVie());
 
     }
+
+    public void attaque(View view){
+        int actionPkmnAdverse = getActionPkmnAdverse();
+        // Cas où l'adeversaire est en mode défense
+        if(actionPkmnAdverse == 0){
+            int differenceAtkDef = pkmnCourant.getAtk() - pkmnAdverse.getDef();
+            if(differenceAtkDef < 0){
+                pkmnCourant.setVieActuelle(pkmnCourant.getVieActuelle() - differenceAtkDef);
+                ProgressBar progressBarAllie = (ProgressBar) findViewById(R.id.progressBarPokemonAllie);
+                progressBarAllie.setProgress(pkmnCourant.getVieActuelle());
+                TextView textVieAllie = (TextView) findViewById(R.id.textViePokemonAllie);
+                textVieAllie.setText(pkmnCourant.getVieActuelle() + "/" + pkmnCourant.getMaxVie());
+            }
+            if(differenceAtkDef >= 0){
+                pkmnAdverse.setVieActuelle(pkmnAdverse.getVieActuelle() - differenceAtkDef);
+                ProgressBar progressBarAdverse = (ProgressBar) findViewById(R.id.progressBarPokemonAdverse);
+                progressBarAdverse.setProgress(pkmnAdverse.getVieActuelle());
+            }
+        }
+        // Cas où l'adeversaire est en mode attaque
+        if(actionPkmnAdverse == 1){
+            int viePkmnAdverse = pkmnAdverse.getVieActuelle() - pkmnAdverse.getAtk();
+            int viePkmnAllie = pkmnCourant.getVieActuelle() - pkmnCourant.getAtk();
+
+            pkmnAdverse.setVieActuelle(viePkmnAdverse);
+            pkmnCourant.setVieActuelle(viePkmnAllie);
+
+            ProgressBar progressBarAllie = (ProgressBar) findViewById(R.id.progressBarPokemonAllie);
+            progressBarAllie.setProgress(pkmnCourant.getVieActuelle());
+            TextView textVieAllie = (TextView) findViewById(R.id.textViePokemonAllie);
+            textVieAllie.setText(pkmnCourant.getVieActuelle() + "/" + pkmnCourant.getMaxVie());
+
+            ProgressBar progressBarAdverse = (ProgressBar) findViewById(R.id.progressBarPokemonAdverse);
+            progressBarAdverse.setProgress(pkmnAdverse.getVieActuelle());
+        }
+    }
+
+    public void defense(View view){
+        int actionPkmnAdverse = getActionPkmnAdverse();
+
+        // Cas où l'adeversaire est en mode attaque
+        if(actionPkmnAdverse == 1){
+            int differenceAtkDef = pkmnAdverse.getAtk() - pkmnCourant.getDef();
+            if(differenceAtkDef >= 0){
+                pkmnCourant.setVieActuelle(pkmnCourant.getVieActuelle() - differenceAtkDef);
+                ProgressBar progressBarAllie = (ProgressBar) findViewById(R.id.progressBarPokemonAllie);
+                progressBarAllie.setProgress(pkmnCourant.getVieActuelle());
+                TextView textVieAllie = (TextView) findViewById(R.id.textViePokemonAllie);
+                textVieAllie.setText(pkmnCourant.getVieActuelle() + "/" + pkmnCourant.getMaxVie());
+            }
+            if(differenceAtkDef < 0){
+                pkmnAdverse.setVieActuelle(pkmnAdverse.getVieActuelle() - differenceAtkDef);
+                ProgressBar progressBarAdverse = (ProgressBar) findViewById(R.id.progressBarPokemonAdverse);
+                progressBarAdverse.setProgress(pkmnAdverse.getVieActuelle());
+            }
+        }
+
+        if(pkmnAdverse.getVieActuelle()<0){
+            System.out.println("GAGNE");
+        }
+        if(pkmnCourant.getVieActuelle()<0){
+            System.out.println("PERDU");
+        }
+
+    }
+    public void pokeball(View view){
+
+    }
+    public void fuite(View view){
+
+    }
+
+    public int getActionPkmnAdverse(){
+        int retour;
+        Random randomGenerator = new Random();
+        retour = randomGenerator.nextInt(100)%2;
+        return retour;
+    }
+
 
 }
