d240f3695a8faee3bc5000cccdb946bf557bbf62
tristandefournas <tristan.de-fournas@cpe.fr>
2016-01-05 16:09:34 +0100

    Combat - Récupère les infos depuis la BDD

--- app/src/main/java/fr/pokemonteam/pokemon/controller/CombatActivity.java
+++ app/src/main/java/fr/pokemonteam/pokemon/controller/CombatActivity.java
@@ -10,37 +10,54 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 import fr.pokemonteam.pokemon.R;
+import fr.pokemonteam.pokemon.bdd.Database;
+import fr.pokemonteam.pokemon.model.Pokemon;
+import fr.pokemonteam.pokemon.model.PokemonReel;
+import fr.pokemonteam.pokemon.model.Utilisateur;
 
 public class CombatActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_combat);
 
+        Database db = Database.getInstance(this);
+        Utilisateur u = db.getUser(0);
+
+        System.out.println(u.getNom());
+
+        ArrayList<PokemonReel> monEquipe = u.getEquipe();
+        PokemonReel pkmnCourant = monEquipe.get(0);
+
+        // Définition des infos du pokémon adverse
         ImageView imageViewAdverse = (ImageView) findViewById(R.id.imagePokemonAdverse);
         imageViewAdverse.setImageResource(R.mipmap.magikarp);
-
-        ImageView imageViewAllie = (ImageView) findViewById(R.id.imagePokemonAllie);
-        imageViewAllie.setImageResource(R.mipmap.chrysacier);
 
         ProgressBar progressBarAdverse = (ProgressBar) findViewById(R.id.progressBarPokemonAdverse);
         progressBarAdverse.setMax(100);
         progressBarAdverse.setProgress(100);
 
-        ProgressBar progressBarAllie = (ProgressBar) findViewById(R.id.progressBarPokemonAllie);
-        progressBarAllie.setMax(100);
-        progressBarAllie.setProgress(50);
-
         TextView textAdverse = (TextView) findViewById(R.id.textPokemonAdverse);
         textAdverse.setText("Magikarp");
 
+        // Définition des infos de notre pokémon
+        ProgressBar progressBarAllie = (ProgressBar) findViewById(R.id.progressBarPokemonAllie);
+        progressBarAllie.setMax(pkmnCourant.getMaxVie());
+        progressBarAllie.setProgress(pkmnCourant.getVieActuelle());
+
         TextView textNomAllie = (TextView) findViewById(R.id.textNomPokemonAllie);
-        textNomAllie.setText("Chrysacier");
+        textNomAllie.setText(pkmnCourant.getPseudo());
+
+        ImageView imageViewAllie = (ImageView) findViewById(R.id.imagePokemonAllie);
+        imageViewAllie.setImageResource(R.mipmap.chrysacier);
 
         TextView textVieAllie = (TextView) findViewById(R.id.textViePokemonAllie);
-        textVieAllie.setText("30/30");
+        textVieAllie.setText(pkmnCourant.getVieActuelle() + "/" + pkmnCourant.getMaxVie());
 
     }
 
